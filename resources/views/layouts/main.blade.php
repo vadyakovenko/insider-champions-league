5496cb38ade7020c87cf220f5fada01d679f0f46
Vadym Yakovenko <vj@consulting.mk>
2020-10-19 21:59:23 +0300

    setup front

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Champions League</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="{{route('home')}}">Insider (Formerly SOCIAPlus) Champions League</a>
+        </div>
+    </nav>
+
+    <main role="main" class="container">
+        @yield('content')
+    </main>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
